3a5bd2af4163ec01146a59d9ee65b727d5e3e324
Filippenko Pavel <filippenko.ps@phystech.edu>
2024-09-20 00:00:38 +0300

    fix bug with remove duplicates

--- second_block/remove_duplicates.cpp
+++ second_block/remove_duplicates.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 #include <vector>
 
-/*
-input:
-0 1 2 3 4 5 6 7
-1 2 3 3 4 5 5 6
-expected out:
-1 2 3 4 5 6 3 5
-*/
-
-//TODO: доделать, пока не работает
+/* Doesn't remove duplicates, just move it to the end of the vector. */
 size_t not_std_uqiue(std::vector<int> *vec, size_t first, size_t last) {
     if (first == last)
         return last;
 
     size_t result = first;
-    while (++first != last) { // если текущий элемент НЕ предпоследний
-        if (!((*vec)[result] == (*vec)[first]) && ++result != first) { // сравниваем текущий и следующий элемент и проверяем, что индекс не один и тот же
-            // меняем местами текущий и последний
+    while (++first != last) {
+        if (!((*vec)[result] == (*vec)[first]) && ++result != first) { 
             auto tmp = (*vec)[first];
             (*vec)[first] = (*vec)[result];
             (*vec)[result] = tmp;
@@ -36,9 +27,28 @@ int main() {
         v.emplace_back(tmp);
     }
 
-    auto last = not_std_uqiue(&v, 0, v.size()-1);
-    for (auto el : v) {
-        std::cout << el << " ";
+    /*
+    Note, that here it's important to put the original size of the vector
+    not the last vector index, that is size - 1.
+    In the original implementation with iterators last is a vector.end() that points 
+    to the position after end.
+
+                  last
+                   |
+                   V
+    1 2 3 4 5 6 7 [ ] 
+    */
+    auto last = not_std_uqiue(&v, 0, v.size());
+    
+    std::cout << "removed duplicates:" << std::endl;
+    for (size_t i = 0; i < last; i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "full vector:" << std::endl;
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
     }
     std::cout << std::endl;
 }
