d1181e4b38c1b4565bb9a23d9509d63b2c951944
Filippenko Pavel <filippenko.ps@phystech.edu>
2025-02-24 13:19:59 +0300

    add internal class

--- 09.07/binary_tree.hpp
+++ 09.07/binary_tree.hpp
@@ -8,68 +8,13 @@ template <typename T>
 class BinaryTree;
 
 template <typename T>
-class Node {
-    public:
-
-    Node(int val)
-        : m_value(val)
-    {
-    }
-
-    template <typename U>
-    friend class BinaryTree;
-
-    private:
-
-    T getValue()
-    {
-        return m_value;
-    }
-
-    std::shared_ptr<Node> getLeftChild()
-    {
-        return m_left;
-    }
-
-    std::shared_ptr<Node> getRightChild()
-    {
-        return m_right;
-    }
-
-    std::weak_ptr<Node> getParent()
-    {
-        return m_parent;
-    }
-
-    void bindLeftChild(std::shared_ptr<Node> left_child)
-    {
-        m_left = left_child;
-    }
-
-    void bindRightChild(std::shared_ptr<Node> right_child)
-    {
-        m_right = right_child;
-    }
-
-    void bindParent(std::weak_ptr<Node> parent)
-    {
-        m_parent = parent;
-    }
-
-    T m_value;
-    std::shared_ptr<Node> m_left;
-    std::shared_ptr<Node> m_right;
-    std::weak_ptr<Node> m_parent;
-};
-
-template <typename T>
 class BinaryTree {
 public:
 
     void insert(int value)
     {
         if (!root) {
-            root = std::make_shared<Node<T> >(value);
+            root = std::make_shared<Node>(value);
         } else {
             insertHelper(root, value);
         }
@@ -82,18 +27,70 @@ public:
     }
 
 private:
-    void insertHelper(std::shared_ptr<Node<T> > node, int value)
+
+    class Node {
+        public:
+
+        Node(int val)
+            : m_value(val)
+        {
+        }
+
+        T getValue()
+        {
+            return m_value;
+        }
+
+        std::shared_ptr<Node> getLeftChild()
+        {
+            return m_left;
+        }
+
+        std::shared_ptr<Node> getRightChild()
+        {
+            return m_right;
+        }
+
+        std::weak_ptr<Node> getParent()
+        {
+            return m_parent;
+        }
+
+        void bindLeftChild(std::shared_ptr<Node> left_child)
+        {
+            m_left = left_child;
+        }
+
+        void bindRightChild(std::shared_ptr<Node> right_child)
+        {
+            m_right = right_child;
+        }
+
+        void bindParent(std::weak_ptr<Node> parent)
+        {
+            m_parent = parent;
+        }
+
+        private:
+
+        T m_value;
+        std::shared_ptr<Node> m_left;
+        std::shared_ptr<Node> m_right;
+        std::weak_ptr<Node> m_parent;
+    };
+
+    void insertHelper(std::shared_ptr<Node> node, int value)
     {
         if (value < node->getValue()) {
             if (!node->getLeftChild()) {
-                node->bindLeftChild(std::make_shared<Node<T> >(value));
+                node->bindLeftChild(std::make_shared<Node>(value));
                 node->getLeftChild()->bindParent(node);
             } else {
                 insertHelper(node->getLeftChild(), value);
             }
         } else {
             if (!node->getRightChild()) {
-                node->bindRightChild(std::make_shared<Node<T> >(value));
+                node->bindRightChild(std::make_shared<Node>(value));
                 node->getRightChild()->bindParent(node);
             } else {
                 insertHelper(node->getRightChild(), value);
@@ -101,7 +98,7 @@ private:
         }
     }
 
-    void inorderHelper(std::shared_ptr<Node<T> > node) const
+    void inorderHelper(std::shared_ptr<Node> node) const
     {
         if (!node)
             return;
@@ -110,7 +107,7 @@ private:
         inorderHelper(node->getRightChild());
     }
 
-    std::shared_ptr<Node<T> > root;
+    std::shared_ptr<Node> root;
 };
 
 #endif // _BINARY_TREE_H
